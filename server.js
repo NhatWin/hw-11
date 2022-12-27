a38adc89e7e1fbdcaca836ea0a1afb27508250c3
NhatWin <nhat0933@gmail.com>
2022-12-27 17:15:39 -0600

    Moved assets folder into public folder

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const noteData = require("./db/db.json")
 
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
 
-app.get("/test", (req, res) => res.json(noteData))
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")))
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
