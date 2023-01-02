0723ef37bd2d234013d5412823197ed6f70ef7a9
NhatWin <nhat0933@gmail.com>
2023-01-01 19:15:22 -0600

    Added post

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ const noteData = require("./db/db.json")
 
 const express = require("express");
 const path = require("path");
+const { writeFile, writeFileSync } = require("fs");
+const { stringify } = require("querystring");
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
 
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")))
+app.use(express.json())
+
+app.get("/api/notes", (_,res) => res.json(noteData));
+
+app.post("/api/notes", (req, res) => {
+  noteData.push(req.body)
+  writeFileSync("./db/db.json", JSON.stringify(noteData));
+  res.json(noteData)
+})
+
+
+// Display
+app.get("/notes", (_, res) => res.sendFile(path.join(__dirname, "./public/notes.html")))
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
